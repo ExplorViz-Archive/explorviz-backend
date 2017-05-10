3b1741b4736f80c971993cf3cb39c21fb282f742
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2017-05-10 15:34:49 +0200

    Updated pushing

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 setup_git() {
+  touch ~/.ssh/id_rsa
+  echo ${deploy-key} > ~/.ssh/id_rsa
+  git config git config core.sshCommand "ssh -i ~/.ssh/id_rsa -F /dev/null" 
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_build_files() {
   git checkout
-  git add . explorviz-ui-backend-*.war
+  cp target/explorviz-ui-backend-*.war explorviz-backend.war  
+  git add explorviz-backend.war
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add https://${GH_TOKEN}@github.com/explorviz/explorviz-docker.git > /dev/null 2>&1
+  git remote add https://git@github.com/explorviz/explorviz-docker.git > /dev/null 2>&1
   git push --quiet > /dev/null 2>&1
 }
 
