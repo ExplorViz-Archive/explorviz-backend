b84aea834d55a871f04d9368d92f4b58a5fbbe99
Alexander Krause <akr@informatik.uni-kiel.de>
2018-08-14 17:13:34 +0200

    wip: exception handling in property injection resolver

--- shared/src/main/java/net/explorviz/shared/annotations/injection/ConfigInjectionResolver.java
+++ shared/src/main/java/net/explorviz/shared/annotations/injection/ConfigInjectionResolver.java
@@ -8,17 +8,24 @@ import javax.ws.rs.InternalServerErrorException;
 import org.glassfish.hk2.api.Injectee;
 import org.glassfish.hk2.api.InjectionResolver;
 import org.glassfish.hk2.api.ServiceHandle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.explorviz.shared.annotations.Config;
 
 @Singleton
 public class ConfigInjectionResolver implements InjectionResolver<Config> {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConfigInjectionResolver.class);
+
 	/*
 	 * private final Properties properties; public
 	 * ConfigurationInjectionResolver(Properties properties) { this.properties =
 	 * properties; }
 	 */
+
+	private final InternalServerErrorException exception = new InternalServerErrorException(
+			"An internal server error occured. Contact your administrator.");
 
 	@Override
 	public Object resolve(Injectee injectee, ServiceHandle<?> root) {
@@ -30,15 +37,22 @@ public class ConfigInjectionResolver implements InjectionResolver<Config> {
 		}
 
 		if ("int".equals(t.toString())) {
-			return Integer.valueOf(String.valueOf(handlePropertyLoading(injectee))).intValue();
+			try {
+				return Integer.valueOf(String.valueOf(handlePropertyLoading(injectee))).intValue();
+			} catch (NumberFormatException e) {
+				LOGGER.error("Property injection for type 'int' failed. Stacktrace:", e);
+				throw exception;
+			}
 		}
 
 		if ("boolean".equals(t.toString())) {
 			return Boolean.valueOf(String.valueOf(handlePropertyLoading(injectee))).booleanValue();
 		}
 
-		throw new InternalServerErrorException(
+		LOGGER.error("Property injection failed: {}",
 				"Type '" + t + "' for property injection is not valid. Use String, int or boolean.");
+		throw exception;
+
 	}
 
 	public Object handlePropertyLoading(Injectee injectee) {
@@ -48,7 +62,9 @@ public class ConfigInjectionResolver implements InjectionResolver<Config> {
 			// return properties.getProperty(prop);
 			return prop;
 		}
-		throw new InternalServerErrorException("Annotation for property injection is not present.");
+		LOGGER.error("Property injection for type 'int' failed: {}",
+				"Annotation for property injection is not present.");
+		throw exception;
 	}
 
 	@Override
