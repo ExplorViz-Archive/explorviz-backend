4aad76ed52e4b5265ac0b219150e237f9778c42b
Alexander Krause <akr@informatik.uni-kiel.de>
2018-08-20 15:08:45 +0200

    logging messages onerror or onclosed

--- landscape/src/main/java/net/explorviz/server/helper/BroadcastService.java
+++ landscape/src/main/java/net/explorviz/server/helper/BroadcastService.java
@@ -9,6 +9,8 @@ import javax.ws.rs.sse.SseBroadcaster;
 import javax.ws.rs.sse.SseEventSink;
 
 import org.jvnet.hk2.annotations.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.explorviz.model.landscape.Landscape;
 
@@ -16,6 +18,7 @@ import net.explorviz.model.landscape.Landscape;
 @Singleton
 public class BroadcastService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BroadcastService.class);
 	private static final MediaType APPLICATION_JSON_API_TYPE = new MediaType("application", "vnd.api+json");
 
 	private final Sse sse;
@@ -24,6 +27,9 @@ public class BroadcastService {
 	public BroadcastService(@Context final Sse sse) {
 		this.sse = sse;
 		this.broadcaster = sse.newBroadcaster();
+
+		this.broadcaster.onClose(this::onCloseOperation);
+		this.broadcaster.onError(this::onErrorOperation);
 	}
 
 	public void broadcastMessage(final Landscape landscape) {
@@ -35,6 +41,16 @@ public class BroadcastService {
 
 	public void register(final SseEventSink eventSink) {
 		this.broadcaster.register(eventSink);
+	}
+
+	private void onCloseOperation(final SseEventSink sink) {
+		LOGGER.info("SseEventSink closed");
+	}
+
+	private void onErrorOperation(final SseEventSink sink, final Throwable e) {
+		LOGGER.error(
+				"Broadcasting to a SseEventSink failed. This may not be a problem, since there is no way to unregister.",
+				e);
 	}
 
 }
