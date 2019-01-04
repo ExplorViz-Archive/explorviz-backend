431ab876da616b2728f326abca3f81f4cce8aa10
Alexander Krause <akr@informatik.uni-kiel.de>
2019-01-04 13:01:47 +0100

    wip: well-defined travisci script

---
+++ .travis/docker_push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Building Docker image"
+
+DIRECTORY="$(basename "$PWD")"
+../gradlew assemble
+
+#echo $DOCKER_PW | docker login -u $DOCKER_LOGIN --password-stdin
+
+if [[ "$TRAVIS_BRANCH" == 'dev-1' ]]; then
+  docker build -t explorviz/explorviz-backend-$DIRECTORY:dev .
+  #docker push explorviz/explorviz-backend-$DIRECTORY:dev
+else
+  docker build -t explorviz/explorviz-backend-$DIRECTORY:latest .
+  #docker push explorviz/explorviz-backend-$DIRECTORY:latest
+fi
