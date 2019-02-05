90ea6fa817d17e5d62b1f7ca80ec571c3a686818
Kevin Lotz <lotzk@outlook.com>
2019-02-05 10:44:32 +0100

    Create endpoint for user settings

--- authentication/src/main/java/net/explorviz/security/server/resources/UserSettingsResource.java
+++ authentication/src/main/java/net/explorviz/security/server/resources/UserSettingsResource.java
@@ -1,11 +1,20 @@
 package net.explorviz.security.server.resources;
 
+import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.BadRequestException;
+import javax.inject.Inject;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import net.explorviz.security.services.TokenService;
+import net.explorviz.security.services.UserMongoCrudService;
+import net.explorviz.shared.security.model.TokenDetails;
+import net.explorviz.shared.security.model.User;
 import net.explorviz.shared.security.model.UserSettings;
 
 @Path("v1/settings")
@@ -15,19 +24,59 @@ public class UserSettingsResource {
 
   private static final String MEDIA_TYPE = "application/vnd.api+json";
   private static final String ADMIN_ROLE = "admin";
-  private static final long HARD_CODED_SETTINGS_ID = 1L;
 
+
+  @Inject
+  private UserMongoCrudService userService;
+
+  @Inject
+  TokenService tokenService;
+
+
+  /**
+   * Returns the settings of a user with a given id.
+   *
+   * @param id the user id
+   * @return
+   */
   @GET
   @Path("{id}")
-  @RolesAllowed({ADMIN_ROLE})
+  @PermitAll
   @Produces(MEDIA_TYPE)
-  public UserSettings getDefaultSettings(@PathParam("id") final long id) {
-    if (id == HARD_CODED_SETTINGS_ID) {
-      return new UserSettings();
-    } else {
-      throw new BadRequestException("No settings object with this id.");
+  public UserSettings getSettingsForUser(@PathParam("id") final long id,
+      @Context final HttpHeaders headers) {
+
+
+    final User u = this.userService.getEntityById(id)
+        .orElseThrow(() -> new NotFoundException("User does not exist"));
+
+
+
+    final TokenDetails details = this.tokenService
+        .parseToken(headers.getHeaderString(HttpHeaders.AUTHORIZATION).substring(7));
+
+    System.out.println(details.getUsername());
+
+    if (details.getUserId() != id) {
+      throw new ForbiddenException();
     }
 
+    return u.getSettings();
+
   }
+
+
+
+  /**
+   * Returns the default settings
+   */
+  @GET
+  @RolesAllowed({ADMIN_ROLE})
+  @Produces(MEDIA_TYPE)
+  public UserSettings getDefaultSettings() {
+    return new UserSettings();
+  }
+
+
 
 }
--- authentication/src/test/java/net/explorviz/security/server/resources/UserSettingsResourceEndpointTest.java
+++ authentication/src/test/java/net/explorviz/security/server/resources/UserSettingsResourceEndpointTest.java
@@ -10,6 +10,7 @@ import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.core.Application;
 import net.explorviz.security.server.main.DependencyInjectionBinder;
 import net.explorviz.security.services.TokenService;
+import net.explorviz.security.services.UserMongoCrudService;
 import net.explorviz.security.testutils.TestDatasourceFactory;
 import net.explorviz.shared.security.model.User;
 import net.explorviz.shared.security.model.UserSettings;
@@ -17,9 +18,9 @@ import net.explorviz.shared.security.model.roles.Role;
 import org.eclipse.jetty.http.HttpHeader;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -32,7 +33,7 @@ import xyz.morphia.Datastore;
 @RunWith(MockitoJUnitRunner.class)
 public class UserSettingsResourceEndpointTest extends JerseyTest {
 
-  private static final String URL_PATH = "v1/settings/1";
+  private static final String URL_PATH = "v1/settings/";
   private static final String AUTH_METHOD = "Bearer";
 
   @Inject
@@ -41,17 +42,24 @@ public class UserSettingsResourceEndpointTest extends JerseyTest {
   @Inject
   private TokenService tokenService;
 
+  @Inject
+  private UserMongoCrudService userService;
+
+  @Inject
+  private Datastore datastore;
+
   private final UserSettings userSettings = new UserSettings();
 
   private String adminToken;
   private String normieToken;
+
+
 
   /*
    * Setup
    */
 
   @Override
-  @Before
   public void setUp() throws Exception {
 
     final DependencyInjectionBinder binder = new DependencyInjectionBinder();
@@ -74,8 +82,26 @@ public class UserSettingsResourceEndpointTest extends JerseyTest {
   }
 
   @Override
+  public void tearDown() throws Exception {
+    this.datastore.getCollection(User.class).drop();
+    super.tearDown();
+  }
+
+
+  @Override
   protected Application configure() {
-    return new ResourceConfig(new net.explorviz.security.server.main.Application());
+    final ResourceConfig c =
+        new ResourceConfig(new net.explorviz.security.server.main.Application());
+    c.register(new AbstractBinder() {
+      @Override
+      protected void configure() {
+        this.bind(UserMongoCrudService.class).to(UserMongoCrudService.class).in(Singleton.class)
+            .ranked(10);
+        this.bindFactory(TestDatasourceFactory.class).to(Datastore.class).in(Singleton.class)
+            .ranked(2);
+      }
+    });
+    return c;
   }
 
   /*
@@ -101,5 +127,28 @@ public class UserSettingsResourceEndpointTest extends JerseyTest {
         .get(String.class);
   }
 
+  @Test
+  public void getOwnSettings() {
+
+    final User user = new User("someuser");
+    user.setPassword("abc");
+
+    this.userService.saveNewEntity(user);
+
+    final String token = AUTH_METHOD + " " + this.tokenService.issueNewToken(user);
+
+    System.out.println(this.tokenService.parseToken(token.substring(7)).getUserId());
+
+    final String path = URL_PATH + user.getId();
+
+
+    final byte[] rawResponseBody = this.target(path).request()
+        .header(HttpHeader.AUTHORIZATION.asString(), token).get(byte[].class);
+
+    final UserSettings obtainedUserSettings =
+        this.resourceConverter.readDocument(rawResponseBody, UserSettings.class).get();
+
+    assertEquals("Settings do not match", this.userSettings, obtainedUserSettings);
+  }
 
 }
