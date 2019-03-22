6c61fc87a4fd2384458dc4f8f3fe700e86b8e737
Alexander Krause <akr@informatik.uni-kiel.de>
2019-03-22 12:34:25 +0100

    prod-env-updater for service prefix

--- landscape-service/prod-env-updater.sh
+++ landscape-service/prod-env-updater.sh
@@ -7,3 +7,5 @@ sed -i "s#mongo.host=.*#mongo.host=$MONGO_HOST#g" META-INF/explorviz-custom.prop
 sed -i "s#mongo.port=.*#mongo.port=$MONGO_PORT#g" META-INF/explorviz-custom.properties
 
 sed -i "s#redis.host=.*#redis.host=$REDIS_HOST#g" META-INF/explorviz-custom.properties
+
+sed -i "s#service.prefix=.*#service.prefix=$SERVICE_PREFIX#g" META-INF/explorviz-custom.properties
