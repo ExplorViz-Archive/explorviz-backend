5eba644dd8858c753ed9b082a8e18728159eb51a
Kevin Lotz <lotzk@outlook.com>
2019-04-18 14:49:42 +0200

    Add default settings on application startu

--- settings-service/src/main/java/net/explorviz/settings/server/main/SetupApplicationListener.java
+++ settings-service/src/main/java/net/explorviz/settings/server/main/SetupApplicationListener.java
@@ -1,12 +1,14 @@
 package net.explorviz.settings.server.main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.inject.Inject;
 import javax.servlet.annotation.WebListener;
-import net.explorviz.shared.security.model.User;
-import net.explorviz.shared.security.model.roles.Role;
-import net.explorviz.shared.security.model.settings.UserSettings;
+import net.explorviz.settings.services.MongoRepository;
+import net.explorviz.settings.model.BooleanSetting;
+import net.explorviz.settings.model.DoubleSetting;
+import net.explorviz.settings.model.Setting;
 import org.glassfish.jersey.server.monitoring.ApplicationEvent;
 import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
 import org.glassfish.jersey.server.monitoring.RequestEvent;
@@ -29,6 +31,8 @@ public class SetupApplicationListener implements ApplicationEventListener {
   @Inject
   private Datastore datastore;
 
+  @Inject
+  private MongoRepository<Setting, String> settingRepo;
 
   @Override
   public void onEvent(final ApplicationEvent event) {
@@ -39,7 +43,7 @@ public class SetupApplicationListener implements ApplicationEventListener {
 
 
     if (event.getType().equals(t)) {
-      
+      addDefaultSettings();
     }
 
   }
@@ -51,5 +55,19 @@ public class SetupApplicationListener implements ApplicationEventListener {
 
 
 
+  private void addDefaultSettings() {
+    List<Setting<?>> defaults = new ArrayList<Setting<?>>(Arrays.asList(
+        new BooleanSetting("showFpsCounter", "Show FPS Counter", "\'Frames Per Second\' metrics in visualizations", false),
+        new BooleanSetting("appVizTransparency", "App Viz Transparency", "Transparency effect for selection (left click) in application visualization", true),
+        new BooleanSetting("enableHoverEffects", "Enable Hover Effects", "Hover effect (flashing entities) for mouse cursor", true),
+        new BooleanSetting("keepHighlightingOnOpenOrClose", "Keep Highlighting On Open Or Close", "Toggle if highlighting should be resetted on double click in application visualization", true),
+        new DoubleSetting("appVizCommArrowSize", "Arrow Size in Application Visualization", "Arrow Size for selected communications in application visualization", 1.0),
+        new DoubleSetting("appVizTransparencyIntensity", "Transparency Intensity in Application Visualization", "Transparency effect intensity (\'App Viz Transparency\' must be enabled)", 0.1, 0.5, 0.1)
+      ));
+    
+    defaults.stream()  
+      .filter(d -> !settingRepo.find(d.getId()).isPresent()).forEach(settingRepo::create);
+    
+  }
 
 }
