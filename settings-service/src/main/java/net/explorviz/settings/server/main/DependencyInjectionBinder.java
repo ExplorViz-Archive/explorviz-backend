25fc5474fc0a21eaa3f1b36f3a18c3cd960d437d
Kevin Lotz <lotzk@outlook.com>
2019-05-07 15:17:02 +0200

    Fix DI

--- settings-service/src/main/java/net/explorviz/settings/server/main/DependencyInjectionBinder.java
+++ settings-service/src/main/java/net/explorviz/settings/server/main/DependencyInjectionBinder.java
@@ -7,7 +7,6 @@ import net.explorviz.settings.services.CustomSettingsRepository;
 import net.explorviz.settings.services.CustomSettingsService;
 import net.explorviz.settings.services.MongoRepository;
 import net.explorviz.settings.services.SettingsRepository;
-import net.explorviz.settings.services.mongo.MongoHelper;
 import net.explorviz.shared.common.injection.CommonDependencyInjectionBinder;
 import org.glassfish.hk2.api.TypeLiteral;
 import xyz.morphia.Datastore;
@@ -25,7 +24,6 @@ public class DependencyInjectionBinder extends CommonDependencyInjectionBinder {
     this.bind(SettingsRepository.class).to(new TypeLiteral<MongoRepository<Setting, String>>() {})
         .in(Singleton.class);
 
-    this.bind(MongoHelper.class).to(MongoHelper.class).in(Singleton.class);
     this.bind(SettingsRepository.class).to(SettingsRepository.class).in(Singleton.class);
     this.bind(CustomSettingsRepository.class).to(CustomSettingsRepository.class)
         .in(Singleton.class);
