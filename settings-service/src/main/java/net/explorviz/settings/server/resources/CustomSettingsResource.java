e4e3f1ced166b77e718c589535ee8e1b424f6223
Kevin Lotz <lotzk@outlook.com>
2019-05-09 12:43:45 +0200

    Add missing methods + javadoc to resources

--- settings-service/src/main/java/net/explorviz/settings/server/resources/CustomSettingsResource.java
+++ settings-service/src/main/java/net/explorviz/settings/server/resources/CustomSettingsResource.java
@@ -15,43 +15,70 @@ import net.explorviz.settings.services.CustomSettingsRepository;
 import net.explorviz.settings.services.CustomSettingsService;
 import net.explorviz.settings.services.SettingValidationException;
 
+/**
+ * Resource to access {@link CustomSetting}, i.e. to handle user specific settings.
+ *
+ */
 @Path("v1/settings/custom")
 public class CustomSettingsResource {
 
   private static final String MEDIA_TYPE = "application/vnd.api+json";
 
 
-  private final CustomSettingsRepository repo;
-  private final CustomSettingsService css;
+  private final CustomSettingsRepository customSettingsRepo;
+  private final CustomSettingsService customSettingService;
 
 
+  /**
+   * Creates the endpoint.
+   *
+   * @param customSettingService service for handling user preferences
+   * @param customSettingRepo repository for user preferences
+   */
   @Inject
-  public CustomSettingsResource(final CustomSettingsRepository repo,
-      final CustomSettingsService css) {
+  public CustomSettingsResource(final CustomSettingsRepository customSettingRepo,
+      final CustomSettingsService customSettingService) {
     super();
-    this.repo = repo;
-    this.css = css;
+    this.customSettingsRepo = customSettingRepo;
+    this.customSettingService = customSettingService;
   }
 
+  /**
+   * Access all user settings.
+   *
+   * @return a list of all user settings
+   */
   @GET
   @Produces(MEDIA_TYPE)
   public List<CustomSetting> getAll() {
-    return this.repo.findAll();
+    return this.customSettingsRepo.findAll();
   }
 
+  /**
+   * Access the custom settings of a specific user.
+   *
+   * @param uid the id of the user
+   * @return a list of {@link CustomSetting} describing the users preferences.
+   */
   @GET
   @Produces(MEDIA_TYPE)
   @Path("/{userId}")
   public List<CustomSetting> getForUser(@PathParam("userId") final String uid) {
-    return this.css.getCustomsForUser(uid);
+    return this.customSettingService.getCustomsForUser(uid);
   }
 
+  /**
+   * Creates a new custom setting.
+   *
+   * @param customSetting the custom setting
+   * @return HTTP 200 (OK) iff the custom setting was saved
+   */
   @POST
   @Consumes(MEDIA_TYPE)
-  public Response createCustomSetting(final CustomSetting s) {
+  public Response createCustomSetting(final CustomSetting customSetting) {
     try {
-      this.css.validate(s);
-      this.repo.create(s);
+      this.customSettingService.validate(customSetting);
+      this.customSettingsRepo.create(customSetting);
       return Response.ok().build();
     } catch (final SettingValidationException e) {
       // TODO Auto-generated catch block
--- settings-service/src/main/java/net/explorviz/settings/server/resources/SettingsInfoResource.java
+++ settings-service/src/main/java/net/explorviz/settings/server/resources/SettingsInfoResource.java
@@ -2,7 +2,9 @@ package net.explorviz.settings.server.resources;
 
 import java.util.List;
 import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
@@ -13,6 +15,10 @@ import javax.ws.rs.core.Response;
 import net.explorviz.settings.model.Setting;
 import net.explorviz.settings.services.SettingsRepository;
 
+/**
+ * API for handling {@link Setting}s and their associated information.
+ *
+ */
 @Path("v1/settings/info")
 public class SettingsInfoResource {
 
@@ -26,12 +32,24 @@ public class SettingsInfoResource {
     this.repo = repo;
   }
 
+  /**
+   * Endpoint to access all settings.
+   *
+   * @return all settings currently available.
+   */
   @GET
   @Produces(MEDIA_TYPE)
   public List<Setting> getAll() {
     return this.repo.findAll();
   }
 
+  /**
+   * Endpoint to access a single setting.
+   *
+   * @param id the id of the setting
+   * @return the setting with HTTP 200 (OK)
+   * @throws NotFoundException if there is no setting with the given id
+   */
   @GET
   @Produces(MEDIA_TYPE)
   @Path("/{id}")
@@ -39,11 +57,37 @@ public class SettingsInfoResource {
     return this.repo.find(id).orElseThrow(NotFoundException::new);
   }
 
+  /**
+   * Endpoint to delete a single setting.
+   *
+   * @param id the id of the setting
+   * @return returns a HTTP Status 201 (No Content).
+   */
+  @DELETE
+  @Produces(MEDIA_TYPE)
+  @Path("/{id}")
+  public Response deleteById(@PathParam("id") final String id) {
+    this.repo.delete(id);
+    return Response.noContent().build();
+  }
+
+  /**
+   * Endpoint for creation of new settings.
+   *
+   * @param s a setting more specific a subtype of {@link Setting}. @return Returns HTTP 200 (OK) on
+   *        success
+   * @throws BadRequestException if the setting is malformed and could not be saved
+   */
   @POST
   @Consumes(MEDIA_TYPE)
   public Response createSetting(final Setting s) {
-    this.repo.create(s);
-    return Response.ok().build();
+    try {
+      this.repo.create(s);
+      return Response.ok().build();
+    } catch (final Exception e) {
+      throw new BadRequestException();
+    }
+
   }
 
 
