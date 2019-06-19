1bcd2df6e7e53e0dbc6968365e8fb655b44c1543
Alexander Krause <akr@informatik.uni-kiel.de>
2019-06-19 10:15:32 +0200

    tests for PaginationFilter

--- user-service/src/main/java/net/explorviz/security/server/filter/PaginationFilter.java
+++ user-service/src/main/java/net/explorviz/security/server/filter/PaginationFilter.java
@@ -1,9 +1,11 @@
 package net.explorviz.security.server.filter;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -18,49 +20,86 @@ public class PaginationFilter implements ContainerResponseFilter {
   private static final String PAGE_QUERY_PARAM_NAME = "page";
   private static final String PER_PAGE_QUERY_PARAM_NAME = "per_page";
 
-  private static final int DEFAULT_PAGE_VALUE = 0;
+  private static final String QUERY_EXCEPTION_MESSAGE =
+      "Query parameter '{0}' has to be a non-negative integer value.";
+
+  private static final int MIN_PAGE_VALUE = 1;
+  private static final int MIN_PER_PAGE_VALUE = 1;
+
+  private static final int PAGE_CALC_OFFSET = 1;
+
+  private static final int DEFAULT_PAGE_VALUE = 1;
   private static final int DEFAULT_PER_PAGE_VALUE = 100;
 
   private List<Object> finalResultList = new ArrayList<>();
 
   @Override
-  public void filter(final ContainerRequestContext requestContext,
+  public void filter(final ContainerRequestContext requestContext, // NOPMD
       final ContainerResponseContext responseContext) throws IOException {
 
     final Object entity = responseContext.getEntity();
 
     // only proceed if response entity is of type list
-    if (entity != null && entity instanceof List) {
-
-      @SuppressWarnings("unchecked")
-      final List<Object> entityList = (List<Object>) entity;
-
-      final MultivaluedMap<String, String> queryParams =
-          requestContext.getUriInfo().getQueryParameters();
-
-      final String pageParam = queryParams.getFirst(PAGE_QUERY_PARAM_NAME);
-      int pageParamValue = DEFAULT_PAGE_VALUE;
-
-      if (pageParam != null) {
-        pageParamValue = Integer.valueOf(pageParam);
-      }
-
-      final String perPageParam = queryParams.getFirst(PER_PAGE_QUERY_PARAM_NAME);
-      int perPageParamValue = DEFAULT_PER_PAGE_VALUE;
-
-      if (perPageParam != null) {
-        perPageParamValue = Integer.valueOf(perPageParam);
-      }
-
-      final List<List<Object>> resultList = this.splitListIntoChunks(entityList, perPageParamValue);
-
-      if (pageParamValue <= resultList.size()) {
-        this.finalResultList = resultList.get(pageParamValue);
-      } else {
-        this.finalResultList = resultList.get(0);
-      }
-      responseContext.setEntity(this.finalResultList);
+    if (!(entity instanceof List)) {
+      return;
     }
+
+    final MultivaluedMap<String, String> queryParams =
+        requestContext.getUriInfo().getQueryParameters();
+
+    // Get potential pagination page
+    final String pageParam = queryParams.getFirst(PAGE_QUERY_PARAM_NAME);
+    int pageParamValue = DEFAULT_PAGE_VALUE;
+
+    if (pageParam != null) {
+      try {
+        pageParamValue = Integer.valueOf(pageParam);
+
+        if (pageParamValue < MIN_PAGE_VALUE) { // NOPMD
+          throw new NumberFormatException();
+        }
+      } catch (final NumberFormatException e) {
+        final String exceptionMessage =
+            MessageFormat.format(QUERY_EXCEPTION_MESSAGE, PAGE_QUERY_PARAM_NAME);
+        throw new BadRequestException(exceptionMessage, e);
+      }
+    }
+
+    // Get potential pagination per_page
+    final String perPageParam = queryParams.getFirst(PER_PAGE_QUERY_PARAM_NAME);
+    int perPageParamValue = DEFAULT_PER_PAGE_VALUE;
+
+    if (perPageParam != null) {
+      try {
+        perPageParamValue = Integer.valueOf(perPageParam);
+
+        if (perPageParamValue < MIN_PER_PAGE_VALUE) { // NOPMD
+          throw new NumberFormatException();
+        }
+      } catch (final NumberFormatException e) {
+        final String exceptionMessage =
+            MessageFormat.format(QUERY_EXCEPTION_MESSAGE, PER_PAGE_QUERY_PARAM_NAME);
+        throw new BadRequestException(exceptionMessage, e);
+      }
+    }
+
+    // build resultList
+    @SuppressWarnings("unchecked")
+    final List<Object> entityList = (List<Object>) entity;
+
+    final List<List<Object>> resultList = this.splitListIntoChunks(entityList, perPageParamValue);
+
+    final int pageToListIndex = pageParamValue - PAGE_CALC_OFFSET;
+
+    if (pageToListIndex <= resultList.size()) {
+      this.finalResultList = resultList.get(pageToListIndex);
+    } else {
+      // page number is higher than actual page maximum -> return no entities
+      this.finalResultList = new ArrayList<>();
+    }
+
+    // finally modify response entity
+    responseContext.setEntity(this.finalResultList);
   }
 
   private List<List<Object>> splitListIntoChunks(final List<Object> listToSplit,
--- user-service/src/test/java/net/explorviz/security/server/filter/PaginationFilterTest.java
+++ user-service/src/test/java/net/explorviz/security/server/filter/PaginationFilterTest.java
@@ -1,10 +1,12 @@
 package net.explorviz.security.server.filter;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.io.IOException;// NOCS
+import static org.junit.jupiter.api.Assertions.assertEquals; // NOCS
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.IOException; // NOCS
 import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.core.MultivaluedHashMap;
@@ -13,8 +15,6 @@ import javax.ws.rs.core.UriInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -24,14 +24,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
  *
  */
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
-@SuppressWarnings("PMD")
 public class PaginationFilterTest {
 
   private static final String PAGE_QUERY_PARAM_NAME = "page";
   private static final String PER_PAGE_QUERY_PARAM_NAME = "per_page";
 
   private static final int NUMBER_OF_ENTITIES = 1000;
+
+  private static final String ERROR_LIST_MESSAGE = "Wrong list was returned.";
 
   @Mock
   private ContainerRequestContext requestContext;
@@ -41,9 +41,9 @@ public class PaginationFilterTest {
 
   private PaginationFilter filter;
 
-  private List<Integer> responseList;
+  private List<Integer> responseList; // NOPMD
 
-  @BeforeEach
+  @BeforeEach // NOCS
   public void setUp() {
 
     this.responseList = new ArrayList<>();
@@ -65,9 +65,22 @@ public class PaginationFilterTest {
   }
 
   @Test
+  public void testDefaultForNoParams() throws IOException {
+    // no query param -> first page, 100 entities -> 0 to 99
+    final MultivaluedMap<String, String> queryParams = new MultivaluedHashMap<>();
+
+    Mockito.when(this.requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParams);
+
+    this.filter.filter(this.requestContext, this.responseContext);
+
+    final List<Integer> expectedList = this.giveListWithIntegers(0, 100);
+
+    assertEquals(expectedList, this.filter.getFinalResultList(), ERROR_LIST_MESSAGE);
+  }
+
+  @Test
   public void testPartialPage() throws IOException {
-    // first page, 30 entities
-    // filter does not return
+    // query param third page, 30 entities -> 60 to 89
     final MultivaluedMap<String, String> queryParams = new MultivaluedHashMap<>();
     queryParams.putSingle(PAGE_QUERY_PARAM_NAME, "3");
     queryParams.putSingle(PER_PAGE_QUERY_PARAM_NAME, "30");
@@ -78,7 +91,59 @@ public class PaginationFilterTest {
 
     final List<Integer> expectedList = this.giveListWithIntegers(60, 30);
 
-    assertEquals(expectedList, this.filter.getFinalResultList());
+    assertEquals(expectedList, this.filter.getFinalResultList(), ERROR_LIST_MESSAGE);
+  }
+
+  @Test
+  public void testNegativePageQueryParam() throws IOException {
+    // query param '-1' page, 30 entities -> BadRequestException
+    final MultivaluedMap<String, String> queryParams = new MultivaluedHashMap<>();
+    queryParams.putSingle(PAGE_QUERY_PARAM_NAME, "-1");
+    queryParams.putSingle(PER_PAGE_QUERY_PARAM_NAME, "30");
+
+    Mockito.when(this.requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParams);
+
+    assertThrows(BadRequestException.class,
+        () -> this.filter.filter(this.requestContext, this.responseContext));
+  }
+
+  @Test
+  public void testNonIntegerPageQueryParam() throws IOException {
+    // query param 'true' page, 30 entities -> BadRequestException
+    final MultivaluedMap<String, String> queryParams = new MultivaluedHashMap<>();
+    queryParams.putSingle(PAGE_QUERY_PARAM_NAME, "true");
+    queryParams.putSingle(PER_PAGE_QUERY_PARAM_NAME, "30");
+
+    Mockito.when(this.requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParams);
+
+    assertThrows(BadRequestException.class,
+        () -> this.filter.filter(this.requestContext, this.responseContext));
+  }
+
+  @Test
+  public void testNonPositivePerPageQueryParam() throws IOException {
+    // query param first page, 0 entities -> BadRequestException
+    final MultivaluedMap<String, String> queryParams = new MultivaluedHashMap<>();
+    queryParams.putSingle(PAGE_QUERY_PARAM_NAME, "1");
+    queryParams.putSingle(PER_PAGE_QUERY_PARAM_NAME, "0");
+
+    Mockito.when(this.requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParams);
+
+    assertThrows(BadRequestException.class,
+        () -> this.filter.filter(this.requestContext, this.responseContext));
+  }
+
+  @Test
+  public void testNonIntegerPerPageQueryParam() throws IOException {
+    // query param first page, 'true' entities -> BadRequestException
+    final MultivaluedMap<String, String> queryParams = new MultivaluedHashMap<>();
+    queryParams.putSingle(PAGE_QUERY_PARAM_NAME, "1");
+    queryParams.putSingle(PER_PAGE_QUERY_PARAM_NAME, "true");
+
+    Mockito.when(this.requestContext.getUriInfo().getQueryParameters()).thenReturn(queryParams);
+
+    assertThrows(BadRequestException.class,
+        () -> this.filter.filter(this.requestContext, this.responseContext));
   }
 
   private List<Integer> giveListWithIntegers(final int startInteger,
